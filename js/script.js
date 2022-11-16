73ebc16a01545a185012210e0ed54ea8b96f109b
Ashot2497 <96375151+Ashot2497@users.noreply.github.com>
2022-11-16 19:50:23 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let image = document.querySelectorAll('.level_middle img');
+let text = document.querySelectorAll('.level_middle .text');
+console.log(text)
+
+for (let i = 0; i < image.length; i++) {
+   image[i].onmouseenter = f1;
+   image[i].onmouseleave = f2;
+
+}
+function f1() {
+}
+function f2() {
+
+}
